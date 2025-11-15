5521c78a67460e0f481803c50b2c83fd6bb772f3
ZF <zahirfarooq984@gmail.com>
2025-11-15 09:48:03 +0000

    Added all python exercises file for chapter 2 - variables

--- python/Ch2-Variables/Ch2-Variables.py
+++ python/Ch2-Variables/Ch2-Variables.py
@@ -1,1 +1,9 @@
-print('Hello from Ch2-Variables!')
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+# Python Crash Course Chapter 2 - Variables
+# Covers the use of creating simple programs
+# setting variables and printing them 
